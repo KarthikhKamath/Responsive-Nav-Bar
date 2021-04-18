6d91ebe3cb7646362d6c875e392bd13952ee672b
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2021-04-18 20:57:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+document.querySelector(".hamburger-menu").addEventListener("click", () =>{
+    document.querySelector(".navbar ").classList.toggle("change")
+})
+
+document.querySelector(".hamburger-menu").addEventListener("click", () =>{
+    document.querySelector(".links ").classList.toggle("change")
+})
